01ef5db82ed451455022a1cc45986950b9627abf
MOUAD ED-DEKKAK <mouaddekkak34@gmail.com>
2024-01-02 13:14:05 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -131,7 +131,7 @@ const Footer = () => {
 
             <div className="text-left">
               <Typography placeholder={undefined} color="blue-gray" className="font-normal">
-                &copy; 2023 LeyYer Tech OÜ
+                &copy; 2023 Leyyer Tech OÜ
               </Typography>
             </div>
           </div>
